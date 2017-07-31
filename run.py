3f7564f4768721ae5f473bdebd5c4ff6aef91db3
mmmaddddy <maddyvarner@gmail.com>
2017-07-31 17:46:04 -0400

    fuck my life

--- run.py
+++ run.py
@@ -46,7 +46,7 @@ def discuss():
         if body == 'test':
             print "testing this situation outgit"
             print from_number, state, 'where_were_u'
-            respond.delay(from_number, state, 'where_were_u', 'testing 1, 2, 3')#, countdown=5.0)
+            respond.apply_async((from_number, state, 'where_were_u', 'testing 1, 2, 3'), countdown=5.0)
         elif state == 'where_were_u':
             if question:
                 (new_state, response) = formulate_answer(body)
